2905cd4c4caf309c69f8324b554a7852c2abfc0f
Хазова Александра <72195557+Khazovalexandra@users.noreply.github.com>
2020-10-26 15:00:52 +0300

    Add files via upload

---
+++ proverka1.py
@@ -0,0 +1,12 @@
+from os import walk
+
+def rec(dir):
+    for (dirpath, dirnames, files) in walk(dir):
+        for file in files:
+            print(file)
+        if len(dirnames) != 0:
+            for i in range(len(dirnames)):
+                rec(dir+'\\'+dirnames[i])
+        return 0
+
+rec('C:/Users/Сергей/PycharmProjects/pythonProject1/proverka1.py')
